1b0ffab41bf64687f4943493d1e2d0a0e14f983b
filipecsoares <filipecarlossoares@gmail.com>
2023-07-12 17:04:58 -0300

    feat: generate token

--- domain/credential_model.go
+++ domain/credential_model.go
@@ -1,6 +1,12 @@
 package domain
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 type Credential struct {
 	ID           CredentialID `json:"id"`
@@ -35,4 +41,47 @@ func validate(params SaveParams) error {
 		return fmt.Errorf("password must be at least 8 characters")
 	}
 	return nil
+}
+
+func (a *Credential) HashPassword(hasher Hasher, password string) error {
+	hash, err := hasher.Hash(password)
+	if err != nil {
+		return fmt.Errorf("hash password failed %v", err)
+	}
+
+	a.PasswordHash = hash
+	return nil
+}
+
+func (a *Credential) GenerateAuthToken() error {
+	h := sha256.New()
+	if _, err := h.Write([]byte(fmt.Sprintf("%d-%s", time.Now().Unix(), randString()))); err != nil {
+		return fmt.Errorf("sha256 write for token id failed %v", err)
+	}
+
+	a.AuthToken = AuthToken{
+		ID:        AuthTokenID(hex.EncodeToString(h.Sum(nil))),
+		ExpiresAt: time.Now().Add(24 * time.Hour),
+	}
+
+	return nil
+}
+
+func (a *Credential) AuthTokenExpired() bool {
+	return a.AuthToken.ExpiresAt.Before(time.Now())
+}
+
+func (a *Credential) ExpireAuthToken() {
+	a.AuthToken = AuthToken{ID: "-", ExpiresAt: time.Now().Add(-(time.Hour * 24))}
+}
+
+const charset = "jhfkjhdfsJHKJHkjhk98798jkhkjhpoqu"
+
+func randString() string {
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	b := make([]byte, 12)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
 }
